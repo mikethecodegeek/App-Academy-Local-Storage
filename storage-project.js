8b270fa037e2a8b065c1f8f6a4da162e894f1d53
leoworkcp <leoworkcp@gmail.com>
2020-10-01 13:46:45 -0400

    first work done

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,29 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  const item = document.getElementById("items");
+  const quantity = document.getElementById("quantity");
+  const cart = document.getElementById("shopping-cart");
+  const button = document.getElementById("add-to-cart");
 
-  // const showCart = () => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    storeItem(item.value, quantity.value);
+    const p = document.createElement("p");
+    p.innerHTML = `${item.value} ${quantity.value} `;
+    cart.appendChild(p);
+  });
 
-  // };
+  const showCart = () => {
+    Object.keys(localStorage).forEach((key) => {
+      const p = document.createElement("p");
+      p.innerHTML = `${key} ${JSON.parse(localStorage.getItem(key))} `;
+      cart.appendChild(p);
 
-  // const storeItem = () => {
-
-  // };
-
-  // const removeItem = () => {
-
-  // };
-
+      // console.log(key);
+    });
+  };
+  showCart();
+  const storeItem = (item, quantity) => {
+    localStorage.setItem(item, JSON.stringify(quantity));
+  };
+  const removeItem = () => {};
 });
